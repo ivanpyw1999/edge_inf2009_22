e908eea133e1b4d75f077d35244dd97240d95fa7
2202433 <122218696+ivanpyw1999@users.noreply.github.com>
2025-03-27 15:40:06 +0800

    added some statistics

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask import request, jsonify
 
 from extensions import db
+from sqlalchemy import func
 
 from models.db import Product
 from models.db import AdvertisementTargeting
@@ -15,17 +16,51 @@ db.init_app(app)
 
 @app.route("/")
 def homepage():
+    # Total advertisements shown (sum of counters)
+    total_ads = db.session.query(func.sum(AdvertisementTargeting.counter)).scalar() or 0
+
+    # Top 5 advertisements by counter
+    top_ads_records = AdvertisementTargeting.query.order_by(AdvertisementTargeting.counter.desc()).limit(5).all()
+    top_ads = []
+    for ad in top_ads_records:
+        product = Product.query.get(ad.product_id)
+        if product:
+            top_ads.append({'name': product.name, 'counter': ad.counter})
+
+    # Most popular item (highest counter)
+    most_pop_ad = AdvertisementTargeting.query.order_by(AdvertisementTargeting.counter.desc()).first()
+    popular = None
+    if most_pop_ad:
+        product = Product.query.get(most_pop_ad.product_id)
+        if product:
+            popular = {'name': product.name, 'counter': most_pop_ad.counter}
+
+    # Least popular item (lowest counter)
+    least_pop_ad = AdvertisementTargeting.query.order_by(AdvertisementTargeting.counter.asc()).first()
+    least = None
+    if least_pop_ad:
+        product = Product.query.get(least_pop_ad.product_id)
+        if product:
+            least = {'name': product.name, 'counter': least_pop_ad.counter}
+
+    # List all products for testing
     products = Product.query.all()
-    result = [
-        {
+    products_result = [
+         {
             'id': product.id,
             'name': product.name,
             'category': product.category,
-            # Not returning image binary data in this example.
-        }
-        for product in products
+         }
+         for product in products
     ]
-    return render_template("dashboard_statistics.html", products=result)
+    return render_template(
+         "dashboard_statistics.html",
+         products=products_result,
+         total_ads=total_ads,
+         top_ads=top_ads,
+         popular=popular,
+         least=least
+    )
 
 
 @app.route("/upload")
